47c42a45ad43fe5d03f3bd34c5ccef0dbba6ccab
ArtTheBeing <dmccord9912@gmail.com>
2023-08-04 14:59:13 -0500

    start

---
+++ server.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+import os
+import openai
+from sqlalchemy import *
+from sqlalchemy.orm import relationship
+
+app = Flask(__name__)
+
+##Connect to Database
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('POSTGRES')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password=db.Column(db.String(200), nullable=False)
+    #flashcards = relationship('Petition', back_populates='author')
+    
+#hello

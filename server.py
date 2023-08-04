6644706395c44aa84cdd364be692de17d5256d67
murphymy04 <137734306+murphymy04@users.noreply.github.com>
2023-08-04 15:54:42 -0500

    create user endpoint

--- server.py
+++ server.py
@@ -4,11 +4,12 @@ import os
 import openai
 from sqlalchemy import *
 from sqlalchemy.orm import relationship
+from sqlalchemy_utils import JSONType
 
 app = Flask(__name__)
 
 ##Connect to Database
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('POSTGRES')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flashcards.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 db = SQLAlchemy(app)
@@ -18,6 +19,30 @@ class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password=db.Column(db.String(200), nullable=False)
-    #flashcards = relationship('Petition', back_populates='author')
-    print('hello')
-#hello
+    flashcards = relationship('AllFlashCards', back_populates='author')
+    def to_dict(self):
+        return{c.name: getattr(self, c.name) for c in self.__table__.columns}    
+    
+class AllFlashCards(db.Model):
+    __tablename__ = 'flashcards'
+    id = db.Column(db.Integer, primary_key=True)
+    flashcards = db.Column(JSONType)
+    author = relationship('Users', back_populates='flashcards')
+    author_id = db.Column(db.String(100), ForeignKey('users.id'))
+
+    def to_dict(self):
+        return{c.name: getattr(self, c.name) for c in self.__table__.columns}
+    
+    
+@app.route("/create_user", methods=["GET", "POST"])
+def user():
+    content = request.json
+    new_user = Users(
+        username = content['username'],
+        password = content['password'],
+    )
+    db.session.add(new_user)
+    db.session.commit()
+
+
+
